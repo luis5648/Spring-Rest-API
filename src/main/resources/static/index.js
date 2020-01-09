041e581d6593af92f20a90260571f9365088b4f1
Luis Román <porta5648@gmail.com>
2020-01-09 15:39:44 -0600

    POST con xmlhttpRequest

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,8 +1,36 @@
 /**
  * 
  */
+let enviar = document.getElementById('enviar_reg');
 
-console.log("aaaaaaaaah");
-var aaa = document.querySelector("#unawea");
+var id = document.querySelector('#ID_USUARIO').value;
+var user = document.querySelector('#NOMBRE_USUARIO').value;
+var pass = document.getElementById('PASS').value;
 
-aaa.innerHTML+="<p>aaaaah</p>";
+let datos = {
+	id_USUARIO:id,
+	nombre_USUARIO:user,
+	pass: pass
+}
+
+enviar.addEventListener('click', function(){
+
+	console.log(user);
+	console.log(id);
+	console.log(pass);
+
+
+	const HTTP = new XMLHttpRequest();
+	const url = 'http://192.168.1.102:8080/users';
+
+	
+	HTTP.open("POST",url);
+	HTTP.setRequestHeader("Content-Type", "application/json");
+	HTTP.send(JSON.stringify(datos));
+	
+	/*
+	HTTP.onreadystatechange=(e)=>{
+		console.log(HTTP.);
+	}*/
+});
+
