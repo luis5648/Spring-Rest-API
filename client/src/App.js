7c8de535638814cd982172e8ee900577ce52a1f0
Luis Román <porta5648@gmail.com>
2020-01-14 19:10:09 -0600

    Archivos del cliente en Reactjs

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+
+
+// function App() {
+// 	return (
+// 		<div className="App">
+// 		 <h1>Componente App de react!</h1>
+// 		</div>
+// 	);
+// }
+
+class App extends Component{
+	render(){
+		return(
+			<div>
+				<h1>Componente</h1>
+			</div>
+		);
+	}
+}
+
+
+
+export default App;
